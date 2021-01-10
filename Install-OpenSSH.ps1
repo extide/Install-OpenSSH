1327a05080025f89648c86daaf9a9f3d24ae69ab
James Fry <James.Fry@teraknor.net>
2021-01-10 14:43:18 -0700

    Added $ErrorActionPreference

--- Install-OpenSSH.ps1
+++ Install-OpenSSH.ps1
@@ -5,6 +5,7 @@ $DisablePubkeyAuthentication = $True
 #These ones probably should not change
 $GitUrl = 'https://github.com/PowerShell/Win32-OpenSSH/releases/latest/'
 $GitZipName = "OpenSSH-Win64.zip"
+$ErrorActionPreference = "Stop"
 
 # Detect Elevation:
 $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -42,53 +43,53 @@ $OpenSSHURL = $([String]$response.GetResponseHeader("Location")).Replace('tag', 
 #Download and extract archive
 Write-Host "Downloading Archive" -ForegroundColor Green
 Invoke-WebRequest -Uri $OpenSSHURL -OutFile $GitZipName -ErrorAction Stop
-Write-Host "Download Complete, now expanding and copying to destination" -ForegroundColor Green
+Write-Host "Download Complete, now expanding and copying to destination" -ForegroundColor Green -ErrorAction Stop
 Expand-Archive $GitZipName -DestinationPath . -Force -ErrorAction Stop
 Remove-Item -Path $GitZipName -Force -ErrorAction SilentlyContinue
 Remove-Item -Path $InstallPath -Force -Recurse -ErrorAction SilentlyContinue
-New-Item -Path $InstallPath -ItemType "directory" | Out-Null
-Move-Item -Path .\OpenSSH-Win64\* -Destination $InstallPath
+New-Item -Path $InstallPath -ItemType "directory" -ErrorAction Stop | Out-Null
+Move-Item -Path .\OpenSSH-Win64\* -Destination $InstallPath -ErrorAction Stop
 Remove-Item -Path .\OpenSSH-Win64 -Force -ErrorAction SilentlyContinue
 
 #Do Install
 Write-Host "Running Install Commands" -ForegroundColor Green
-Set-Location $InstallPath
+Set-Location $InstallPath -ErrorAction Stop
 powershell.exe -ExecutionPolicy Bypass -File install-sshd.ps1
-Set-Service -Name sshd -StartupType 'Automatic'
+Set-Service -Name sshd -StartupType 'Automatic' -ErrorAction Stop
 
 #Make sure your ProgramData\ssh directory exists
 If (!(Test-Path $env:ProgramData\ssh)) {
     Write-Host "Creating ProgramData\ssh directory" -ForegroundColor Green
-    New-Item -ItemType Directory -Force -Path $env:ProgramData\ssh | Out-Null
+    New-Item -ItemType Directory -Force -Path $env:ProgramData\ssh -ErrorAction Stop | Out-Null
 }
 
 #Setup sshd_config
 Write-Host "Configure server config file" -ForegroundColor Green
-Copy-Item -Path $InstallPath\sshd_config_default -Destination $env:ProgramData\ssh\sshd_config -Force
-Add-Content -Path $env:ProgramData\ssh\sshd_config -Value "GSSAPIAuthentication yes"
-if ($DisablePasswordAuthentication) { Add-Content -Path $env:ProgramData\ssh\sshd_config -Value "PasswordAuthentication no" }
-if ($DisablePubkeyAuthentication) { Add-Content -Path $env:ProgramData\ssh\sshd_config -Value "PubkeyAuthentication no" }
+Copy-Item -Path $InstallPath\sshd_config_default -Destination $env:ProgramData\ssh\sshd_config -Force -ErrorAction Stop
+Add-Content -Path $env:ProgramData\ssh\sshd_config -Value "GSSAPIAuthentication yes" -ErrorAction Stop
+if ($DisablePasswordAuthentication) { Add-Content -Path $env:ProgramData\ssh\sshd_config -Value "PasswordAuthentication no" -ErrorAction Stop }
+if ($DisablePubkeyAuthentication) { Add-Content -Path $env:ProgramData\ssh\sshd_config -Value "PubkeyAuthentication no" -ErrorAction Stop }
 
 #Make sure your user .ssh directory exists
 If (!(Test-Path "~\.ssh")) {
     Write-Host "Creating User .ssh directory" -ForegroundColor Green
-    New-Item -ItemType Directory -Force -Path "~\.ssh" | Out-Null
+    New-Item -ItemType Directory -Force -Path "~\.ssh" -ErrorAction Stop | Out-Null
 }
 
 #Set ssh_config
 Write-Host "Configure client config file" -ForegroundColor Green
-Add-Content -Path ~\.ssh\config -Value "GSSAPIAuthentication yes"
+Add-Content -Path ~\.ssh\config -Value "GSSAPIAuthentication yes" -ErrorAction Stop
 
 #Start the service
 Write-Host "Starting Service" -ForegroundColor Green
-Start-Service sshd
+Start-Service sshd -ErrorAction Stop
 
 #Add to path if it isnt already there
 $existingPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
 if ($existingPath -notmatch $InstallPath.Replace("\", "\\")) {
     Write-Host "Adding OpenSSH Directory to path" -ForegroundColor Green
     $newpath = "$existingPath;$InstallPath"
-    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath -ErrorAction Stop
 }
 
 #Make sure user keys are configured correctly
@@ -101,8 +102,9 @@ powershell.exe -ExecutionPolicy Bypass -Command '. .\FixUserFilePermissions.ps1 
 
 #Add firewall rule
 Write-Host "Creating firewall rule" -ForegroundColor Green
-New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22 -ErrorAction SilentlyContinue
 
 #Set Shell to powershell
 Write-Host "Setting default shell to powershell" -ForegroundColor Green
-New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force -ErrorAction Stop
+Write-Hos "Installation completed successfully" -ForegroundColor Green

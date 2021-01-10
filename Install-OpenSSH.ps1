c8e28ebc509201232c0873023ec13178429718ed
James Fry <jamfry@martindoor.com>
2021-01-09 19:32:26 -0700

    # This is a combination of 5 commits. # This is the 1st commit message:
    More readme tweaking
    
    # This is the commit message #2:
    
    More readme tweaks
    
    # This is the commit message #3:
    
    # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    Signed
    
    # This is the commit message #2:
    
    Some minor tweaks
    
    # This is the commit message #4:
    
    Added elevation check and colorised the Write-Host messages
    
    # This is the commit message #5:
    
    Signed

--- Install-OpenSSH.ps1
+++ Install-OpenSSH.ps1
@@ -6,21 +6,34 @@ $DisablePubkeyAuthentication = $True
 $GitUrl = 'https://github.com/PowerShell/Win32-OpenSSH/releases/latest/'
 $GitZipName = "OpenSSH-Win64.zip"
 
+# Detect Elevation:
+$CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$UserPrincipal = New-Object System.Security.Principal.WindowsPrincipal($CurrentUser)
+$AdminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+$IsAdmin = $UserPrincipal.IsInRole($AdminRole)
+
+if ($IsAdmin) {
+    Write-Output "Script is running elevated." -Forecolor Green
+}
+else {
+    throw "Script is not running elevated, which is required. Restart the script from an elevated prompt."
+}
+
 
 #Remove BuiltIn OpenSSH
-Write-Host "Checking for Windows OpenSSH Server"
+Write-Host "Checking for Windows OpenSSH Server" -Forecolor Green
 if ($(Get-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0).State -eq "Installed") {
-    Write-Host "Removing Windows OpenSSH Server"
+    Write-Host "Removing Windows OpenSSH Server" -Forecolor Green
     Remove-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 -ErrorAction SilentlyContinue
 }
-Write-Host "Checking for Windows OpenSSH Client"
+Write-Host "Checking for Windows OpenSSH Client" -Forecolor Green
 if ($(Get-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0).State -eq "Installed") {
-    Write-Host "Removing Windows OpenSSH Client"
+    Write-Host "Removing Windows OpenSSH Client" -Forecolor Green
     Remove-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0 -ErrorAction SilentlyContinue
 }
 
 # Get Upstream URL
-Write-Host "Requesting URL for latest version of OpenSSH"
+Write-Host "Requesting URL for latest version of OpenSSH" -Forecolor Green
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $request = [System.Net.WebRequest]::Create($GitUrl)
 $request.AllowAutoRedirect = $false
@@ -28,9 +41,9 @@ $response = $request.GetResponse()
 $OpenSSHURL = $([String]$response.GetResponseHeader("Location")).Replace('tag', 'download') + "/" + $GitZipName
 
 #Download and extract archive
-Write-Host "Downloading Archive"
+Write-Host "Downloading Archive" -Forecolor Green
 Invoke-WebRequest -Uri $OpenSSHURL -OutFile $GitZipName
-Write-Host "Download Complete, now expanding and copying to destination"
+Write-Host "Download Complete, now expanding and copying to destination" -Forecolor Green
 Expand-Archive $GitZipName -DestinationPath . -Force -ErrorAction Stop
 Remove-Item -Path $GitZipName -Force -ErrorAction SilentlyContinue
 Remove-Item -Path $InstallPath -Force -Recurse -ErrorAction SilentlyContinue
@@ -39,52 +52,58 @@ Move-Item -Path .\OpenSSH-Win64\* -Destination $InstallPath
 Remove-Item -Path .\OpenSSH-Win64 -Force -ErrorAction SilentlyContinue
 
 #Do Install
-Write-Host "Running Install Commands"
+Write-Host "Running Install Commands" -Forecolor Green
 Set-Location $InstallPath
 powershell.exe -ExecutionPolicy Bypass -File install-sshd.ps1
 Set-Service -Name sshd -StartupType 'Automatic'
-Copy-Item -Path $InstallPath\sshd_config_default -Destination $env:ProgramData\ssh\sshd_config -Force
 
-#Set sshd_config
-Write-Host "Configure server config file"
+#Make sure your ProgramData\ssh directory exists
+If (!(Test-Path $env:ProgramData\ssh)) {
+    Write-Host "Creating ProgramData\ssh directory" -Forecolor Green
+    New-Item -ItemType Directory -Force -Path $env:ProgramData\ssh | Out-Null
+}
+
+#Setup sshd_config
+Write-Host "Configure server config file" -Forecolor Green
+Copy-Item -Path $InstallPath\sshd_config_default -Destination $env:ProgramData\ssh\sshd_config -Force
 Add-Content -Path $env:ProgramData\ssh\sshd_config -Value "GSSAPIAuthentication yes"
 if ($DisablePasswordAuthentication) { Add-Content -Path $env:ProgramData\ssh\sshd_config -Value "PasswordAuthentication no" }
 if ($DisablePubkeyAuthentication) { Add-Content -Path $env:ProgramData\ssh\sshd_config -Value "PubkeyAuthentication no" }
 
 #Make sure your user .ssh directory exists
 If (!(Test-Path "~\.ssh")) {
-    Write-Host "Creating User .ssh directory"
+    Write-Host "Creating User .ssh directory" -Forecolor Green
     New-Item -ItemType Directory -Force -Path "~\.ssh" | Out-Null
 }
 
 #Set ssh_config
-Write-Host "Configure client config file"
+Write-Host "Configure client config file" -Forecolor Green
 Add-Content -Path ~\.ssh\config -Value "GSSAPIAuthentication yes"
 
 #Start the service
-Write-Host "Starting Service"
+Write-Host "Starting Service" -Forecolor Green
 Start-Service sshd
 
 #Add to path if it isnt already there
 $existingPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
 if ($existingPath -notmatch $InstallPath.Replace("\", "\\")) {
-    Write-Host "Adding OpenSSH Directory to path"
+    Write-Host "Adding OpenSSH Directory to path" -Forecolor Green
     $newpath = "$existingPath;$InstallPath"
     Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
 }
 
 #Make sure user keys are configured correctly
-Write-Host "Ensuring HostKey file permissions are correct"
+Write-Host "Ensuring HostKey file permissions are correct" -Forecolor Green
 powershell.exe -ExecutionPolicy Bypass -Command '. .\FixHostFilePermissions.ps1 -Confirm:$false'
 
 #Make sure host keys are configured correctly
-Write-Host "Ensuring UserKey file permissions are correct"
+Write-Host "Ensuring UserKey file permissions are correct" -Forecolor Green
 powershell.exe -ExecutionPolicy Bypass -Command '. .\FixUserFilePermissions.ps1 -Confirm:$false'
 
 #Add firewall rule
-Write-Host "Creating firewall rule"
+Write-Host "Creating firewall rule" -Forecolor Green
 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 
 #Set Shell to powershell
-Write-Host "Setting default shell to powershell"
+Write-Host "Setting default shell to powershell" -Forecolor Green
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Install-OpenSSH
+
 I was trying to configure a bunch of Windows Servers to be able to ssh into them using Kerberos, without any password prompts. (Of course this is assuming both the client and server are connected to the same domain, or truested domains) After some googling I was surprised to find out that there isn't a ton of information available on how to do this. Unfortuantely the version of OpenSSH (7.7) that is included with Windows 10 and Windows Server 2019 does not support GSSAPIAuthentication. So to make this work, a newer version of OpenSSH was required. I was not able to find any existing scripts that would "cleanly" do this -- so I decided to make one. Please feel free to create an issue if you have problems, or even submit a pull request if you have made any improvements. This script has, so far, only been tested on Windows 10 20H2 and Windows Server 2019 -- but it should work (possibly requiring some minor modifications) on 2016, and 2012 R2 and possibly 2012 as well. The script is reasonably well documented, so it should be fairly obvious what each section is doing.
 
 
@@ -13,7 +14,8 @@ This script will perform the following actions:
 * Create a firewall rule to allow connection to the service remotely
 * Sets the default shell to powershell
 * Ensures that the permissionf or the Host keys and User keys of current user are correct
-  * This simply executes the FixHostFilePermissions.ps1 and FixUserFilePermissions.ps1 scripts included int he OpenSSH download
+  * This simply executes the FixHostFilePermissions.ps1 and FixUserFilePermissions.ps1 scripts included in the OpenSSH download
+* No mess will be left behind, all temporary downloaded files are automatically cleaned up.
 
 # Configurable Options
 
@@ -61,6 +63,14 @@ There are also 32-bit releases published to this repo -- so if you are attemptin
 
 
 # How to use
+
+#### To quickly install with all defaults, just paste this into an admin powershell window
+
+`cd $env:temp; Invoke-WebRequest -Uri https://raw.githubusercontent.com/extide/Install-OpenSSH/main/Install-OpenSSH.ps1 -OutFile Install-OpenSSH.ps1; .\Install-OpenSSH.ps1`
+
+NOTE: You should *always* examine the powershell script yourself before running it in an admin prompt!
+#### Custom install instructions
+
 * Open an Administrative powershell
 * Download the script with `Invoke-WebRequest -Uri https://raw.githubusercontent.com/extide/Install-OpenSSH/main/Install-OpenSSH.ps1 -OutFile Install-OpenSSH.ps1`
 * Open the script in your favorite editor `code Install-OpenSSH.ps1`

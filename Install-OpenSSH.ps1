9cdda92daed2dd5309849e8f73f7741b69ad46a3
James Fry <James.Fry@teraknor.net>
2021-01-10 21:13:05 -0700

    -Added options to control service autostart -Fixed service stop/remove code -Added Chrome UserAgent to web requests to hopefully make it a bit more reliable -Added random querystring to URL's to ensure they arent cached

--- Install-OpenSSH.ps1
+++ Install-OpenSSH.ps1
@@ -2,17 +2,19 @@
 $InstallPath = "C:\Program Files\OpenSSH"
 $DisablePasswordAuthentication = $True
 $DisablePubkeyAuthentication = $True
+$AutoStartSSHD = $true
+$AutoStartSSHAGENT = $false
 #These ones probably should not change
 $GitUrl = 'https://github.com/PowerShell/Win32-OpenSSH/releases/latest/'
-$GitZipName = "OpenSSH-Win64.zip"
+$GitZipName = "OpenSSH-Win64.zip" #Can use OpenSSH-Win32.zip on older systems
 $ErrorActionPreference = "Stop"
+$UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 
 # Detect Elevation:
 $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $UserPrincipal = New-Object System.Security.Principal.WindowsPrincipal($CurrentUser)
 $AdminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 $IsAdmin = $UserPrincipal.IsInRole($AdminRole)
-
 if ($IsAdmin) {
     Write-Host "Script is running elevated." -ForegroundColor Green
 }
@@ -34,12 +36,12 @@ if ($(Get-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0).State -eq "
 
 #Stop and remove existing services (Perhaps an exisitng OpenSSH install)
 if (Get-Service sshd -ErrorAction SilentlyContinue) {
-    Remove-Service sshd -ErrorAction SilentlyContinue
     Stop-Service sshd -ErrorAction SilentlyContinue
+    sc.exe delete sshd 1>$null
 }
 if (Get-Service ssh-agent -ErrorAction SilentlyContinue) {
-    Remove-Service ssh-agent -ErrorAction SilentlyContinue
     Stop-Service ssh-agent -ErrorAction SilentlyContinue
+    sc.exe delete ssh-agent 1>$null
 }
 
 #Randomize Querystring to ensure our request isnt served from a cache
@@ -53,20 +55,22 @@ $request.AllowAutoRedirect = $false
 $request.Timeout = 5 * 1000
 $request.headers.Add("Pragma", "no-cache")
 $request.headers.Add("Cache-Control", "no-cache")
+$request.UserAgent = $UserAgent
 #if ($null -eq $response -or $null -eq $([String]$response.GetResponseHeader("Location"))) { throw "Unable to download OpenSSH Archive. Sometimes you can get throttled, so just try again later." }
 $OpenSSHURL = $([String]$response.GetResponseHeader("Location")).Replace('tag', 'download') + "/" + $GitZipName
 
-#Also randomize this one...
+# #Also randomize this one...
 $OpenSSHURL += "?random=" + $(Get-Random -Minimum 10000 -Maximum 99999)
 
-#Download and extract archive
+# #Download and extract archive
 Write-Host "Downloading Archive" -ForegroundColor Green
-Invoke-WebRequest -Uri $OpenSSHURL -OutFile $GitZipName -ErrorAction Stop -TimeoutSec 5 -Headers @{"Pragma" = "no-cache"; "Cache-Control" = "no-cache"; }
+Invoke-WebRequest -Uri $OpenSSHURL -OutFile $GitZipName -ErrorAction Stop -TimeoutSec 5 -Headers @{"Pragma" = "no-cache"; "Cache-Control" = "no-cache"; } -UserAgent $UserAgent
 Write-Host "Download Complete, now expanding and copying to destination" -ForegroundColor Green -ErrorAction Stop
 Remove-Item -Path $InstallPath -Force -Recurse -ErrorAction SilentlyContinue
 If (!(Test-Path $InstallPath)) {
     New-Item -Path $InstallPath -ItemType "directory" -ErrorAction Stop | Out-Null
 }
+# $OldEnv = [Environment]::CurrentDirectory
 [Environment]::CurrentDirectory = $(Get-Location)
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 $archive = [System.IO.Compression.ZipFile]::OpenRead($GitZipName)
@@ -78,10 +82,13 @@ $archive.Entries | ForEach-Object {
         [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, $NewFIleName)
     }
 }
+$archive.Dispose()
+[Environment]::CurrentDirectory = $env:temp
 
-#Remove-Item -Path $GitZipName -Force -ErrorAction SilentlyContinue
+#Cleanup zip file
+Remove-Item -Path $GitZipName -Force -ErrorAction SilentlyContinue
 
-#Do Install
+#Run Install Script
 Write-Host "Running Install Commands" -ForegroundColor Green
 Set-Location $InstallPath -ErrorAction Stop
 powershell.exe -ExecutionPolicy Bypass -File install-sshd.ps1
@@ -110,8 +117,18 @@ If (!(Test-Path "~\.ssh")) {
 Write-Host "Configure client config file" -ForegroundColor Green
 Add-Content -Path ~\.ssh\config -Value "GSSAPIAuthentication yes" -ErrorAction Stop
 
+#Setting autostarts
+if ($AutoStartSSHD) {
+    Write-Host "Setting sshd service to Automatic start" -ForegroundColor Green;
+    Set-Service -Name sshd -StartupType Automatic;
+}
+if ($AutoStartSSHAGENT) {
+    Write-Host "Setting ssh-agent service to Automatic start" -ForegroundColor Green;
+    Set-Service -Name ssh-agent -StartupType Automatic;
+}
+
 #Start the service
-Write-Host "Starting Service" -ForegroundColor Green
+Write-Host "Starting sshd Service" -ForegroundColor Green
 Start-Service sshd -ErrorAction Stop
 
 #Add to path if it isnt already there
